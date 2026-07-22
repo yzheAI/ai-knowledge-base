62d3543e4bb3bd9903731e7995f8382499ff4ec8
yuanzhe <3364710079@qq.com>
2026-07-22 14:30:05 +0800

    feat: save retrieval evaluation results to json

--- app/evaluation/run_eval.py
+++ app/evaluation/run_eval.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from pathlib import Path
+import json
 from app.evaluation.evaluator import RetrieverEvaluator
-from app.config import JSON_PATH
+from app.config import JSON_PATH, SAVE_JSON_PATH
 from app.retriever.retriever_adapter import RetrieverAdapter
 from app.core.container import faiss_retriever, bm25_retriever, hybrid_retriever
 
@@ -32,6 +35,30 @@ result_hybrid = evaluator.evaluate(
     h_b_retriever,
 )
 
+results = {
+    "model": "bge-reranker-base",
+    "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "results": {
+        "faiss": result_faiss,
+        "bm25": result_bm25,
+        "hybrid": result_hybrid,
+    }
+}
+
+save_path = Path(
+    SAVE_JSON_PATH
+)
+
+with open(save_path, "w", encoding="utf-8") as f:
+    json.dump(
+        results,
+        f,
+        ensure_ascii=False,
+        indent=4,
+    )
+print(
+    f"Evaluation result saved to {save_path}"
+)
 print(f"Faiss: {result_faiss}")
 print(f"Bm25: {result_bm25}")
 print(f"Hybrid: {result_hybrid}")
--- app/retriever/rerank.py
+++ app/retriever/rerank.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from app.config import RERANK_MODEL_PATH, DEVICE
 
-
+# 全局变量，懒加载
 rerank_model = None
 tokenizer = None
 
@@ -12,6 +12,7 @@ def get_rerank_model():
     global rerank_model, tokenizer
 
     if rerank_model is None:
+        # tokenizer：转换为模型可理解的数字
         tokenizer = AutoTokenizer.from_pretrained(
             RERANK_MODEL_PATH,
             use_fast=False
@@ -22,7 +23,7 @@ def get_rerank_model():
         )
 
         rerank_model.to(DEVICE)
-
+        # 只预测不训练
         rerank_model.eval()
 
     return tokenizer, rerank_model
@@ -32,14 +33,13 @@ def rerank(query, docs, top_k=5):
 
     tokenizer, model = get_rerank_model()
 
-
     pairs = [
         (query, doc["text"])
         for doc in docs
     ]
 
     scores = []
-
+    # 禁止梯度计算
     with torch.no_grad():
 
         for q, text in pairs:
@@ -51,24 +51,24 @@ def rerank(query, docs, top_k=5):
                 truncation=True,
                 return_tensors="pt"
             )
-
+            # 放到设备
             inputs = {
                 k: v.to(DEVICE)
                 for k, v in inputs.items()
             }
-
+            # 模型预测
             outputs = model(**inputs)
-
+            # 模型输出取 score
             score = outputs.logits[0].item()
 
             scores.append(score)
-
+    # 保存分数
     for i, score in enumerate(scores):
         docs[i]["rerank_score"] = float(score)
 
     docs = sorted(
         docs,
-        key=lambda x:x["rerank_score"],
+        key=lambda x: x["rerank_score"],
         reverse=True
     )
 

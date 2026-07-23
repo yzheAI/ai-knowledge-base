003a91de6bd22ee71e707c968bd5e4f3a000b17c
yuanzhe <3364710079@qq.com>
2026-07-23 10:08:58 +0800

    feat: add docker deployment support

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app.include_router(chat_router)
 if __name__ == '__main__':
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        # host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True,
     )
